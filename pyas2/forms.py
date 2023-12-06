4ef96ff2d1e9c69a4211a611a1748e9c4bf7f15c
ipandzic <ivan.pandzic@gmail.com>
2023-12-06 14:53:31 +0100

    Replace old imports

--- pyas2/forms.py
+++ pyas2/forms.py
@@ -1,9 +1,7 @@
 from django.core.exceptions import ValidationError
 
-from pyas2 import models
 from django import forms
-from pyas2 import viewlib
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 HIDDEN_INPUT = forms.widgets.HiddenInput
 
